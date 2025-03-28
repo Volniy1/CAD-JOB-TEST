bdd807afee5d907444dfec285b6e6417cdf2f738
ARTHUR <plankjim3@gmail.com>
2025-03-28 09:08:09 +0300

    small-fix

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -30,5 +30,6 @@ export async function GET() {
     return NextResponse.json({ error: "No submissions yet" }, { status: 404 });
   }
 
-  return NextResponse.json(`Thank you, we will reply, ${lastSubmission.name}`);
+  return NextResponse.json(`Thank you for 
+your interest, ${lastSubmission.name}`);
 }
